c45c072e6b50b661297cff9882be1d11aa0b2ea5
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-06-05 08:32:03 -0700

    feat: genesis

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import sequelize from "./utils/utils.database.js";
+
+import router from "./routes/route.user.js";
+
+const app = express();
+
+app.use("/users", router);
+
+(async () => {
+  try {
+    await sequelize.sync({
+      force: false,
+    });
+    app.listen(process.env.EXTERNAL_PORT || 5000);
+  } catch (error) {
+    console.log(error);
+  }
+})();
